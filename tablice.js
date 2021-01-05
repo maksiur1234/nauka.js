4237aed1094c8f609fc9e91a3d15995bba2e8110
Maksymilian <maksiur1234@gmail.com>
2021-01-05 13:51:26 +0100

    dodanie plikow

---
+++ tablice.js
@@ -0,0 +1,36 @@
+const users = [
+    {
+        name : "piotr", age : 27,
+    },
+    {
+        name : "beata", age : 50
+    },
+    {
+        name : "natka", age : 11
+    },
+    {
+        name : "sławek", age : 53
+    },
+    {
+        name : "maksymilian", age : 19
+    },
+    {
+        name : "piotr", age : 37,
+    },
+    {
+        name : "beata", age : 60
+    },
+    {
+        name : "natka", age : 21
+    },
+    {
+        name : "sławek", age : 63
+    },
+    {
+        name : "maksymilian", age : 29
+    },
+];
+users.forEach(function(dupa){
+    console.log(dupa.name)
+})
+
